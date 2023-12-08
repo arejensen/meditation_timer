7f0d7f2000a435470caf034841b2d746fe2db6ff
Are Jensen <arejensen@gmail.com>
2023-12-08 02:14:44 +0100

    Eliminated some warnings by a bit of restructuring; removed some comments

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 type TimerProps = {
   initialTime: number;
@@ -11,40 +11,40 @@ type TimerProps = {
 const Timer: React.FC<TimerProps> = ({ initialTime, triple, alarmSound, isActive, onTimerFinish }) => {
   const [timeLeft, setTimeLeft] = useState(initialTime);
 
+  const playSound = useCallback(() => {
+    if (triple) {
+      let playCount = 0;
+
+      const playInterval = () => {
+        if (playCount < 3) {
+          const audio = new Audio(alarmSound);
+          audio.play().catch(e => console.error('Error playing sound:', e));
+          playCount++;
+        } else {
+          clearInterval(interval);
+          onTimerFinish();
+        }
+      };
+
+      const interval = setInterval(playInterval, 3500);
+      playInterval();
+    } else {
+      const audio = new Audio(alarmSound);
+      audio.play().then(() => {
+        onTimerFinish();
+      }).catch(e => console.error('Error playing sound:', e));
+    }
+  }, [alarmSound, triple, onTimerFinish]);
+
   useEffect(() => {
     if (!isActive) {
-      setTimeLeft(initialTime); // Reset the timer whenever it becomes inactive
+      setTimeLeft(initialTime);
       return;
     }
 
     const interval = isActive && timeLeft > 0
       ? setInterval(() => setTimeLeft(t => t - 1), 1000)
       : null;
-
-    const playSound = () => {
-      if (triple) {
-        let playCount = 0;
-
-        const playInterval = () => {
-          if (playCount < 3) {
-            const audio = new Audio(alarmSound); // audio source each time due to missing sound when reusing
-            audio.play().catch(e => console.error('Error playing sound:', e));
-            playCount++;
-          } else {
-            clearInterval(interval);
-            onTimerFinish();
-          }
-        };
-
-        const interval = setInterval(playInterval, 3500);
-        playInterval(); // Play the first sound immediately
-      } else {
-        const audio = new Audio(alarmSound);
-        audio.play().then(() => {
-          onTimerFinish();
-        }).catch(e => console.error('Error playing sound:', e));
-      }
-    };
 
     if (timeLeft === 0) {
       playSound(); // Play the sound when the countdown reaches zero
@@ -53,8 +53,7 @@ const Timer: React.FC<TimerProps> = ({ initialTime, triple, alarmSound, isActive
     return () => {
       if (interval) clearInterval(interval);
     };
-  }, [isActive, timeLeft, initialTime]);
-
+  }, [isActive, timeLeft, initialTime, playSound]); 
 
   return (
     <div>
